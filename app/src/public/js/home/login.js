20a71b78dbba814bd213023619b232d3a5c19ee9
patronum98 <zkrhklkl98@gmail.com>
2022-05-12 15:29:32 +0900

    프런트 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -9,7 +9,7 @@ loginBtn.addEventListener("click", login);
 function login() {
     const req = {
         id: id.value,
-        psword: id.value,
+        psword: psword.value,
     };
     fetch("/login", {
         method: "POST",
@@ -17,5 +17,15 @@ function login() {
             "Content-Type": "application/json",
         },
         body: JSON.stringify(req),
-    });
+    })  .then((res) => res.json())
+        .then((res) => {
+            if (res.success) {
+                location.href = "/";
+            }   else {
+                alert(res.msg);
+            }
+        })
+        .catch((err) => {   
+            console.error("로그인 중 에러 발생");
+        });
 }; 

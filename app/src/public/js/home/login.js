95f2b665793cc2afd1b0b36a198ebbbfe0adcdef
patronum98 <zkrhklkl98@gmail.com>
2022-05-13 15:36:04 +0900

    로그인버튼 안뜨는 에러 해결

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -2,9 +2,8 @@
 
 const id = document.querySelector("#id"),
     psword = document.querySelector("#psword"),
-    
     loginBtn = document.querySelector("#button");
-
+    
 loginBtn.addEventListener("click", login);
 
 function login() {

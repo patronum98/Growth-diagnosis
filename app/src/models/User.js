7ebc3e3206891e722910456ef0b3f731c6762f2a
patronum98 <zkrhklkl98@gmail.com>
2022-05-16 15:19:42 +0900

    모든에러 해결, AWS RDS와 연동 & dotenv환경변수 등록

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -1,35 +1,39 @@
 "use strict";
 
+const { response } = require("express");
 const UserStorage = require("./UserStorage");
 
 class User {
-    constructor(body) {
-        this.body = body;
-    }
+  constructor(body) {
+    this.body = body;
+  }
 
-    async login() {
-        const client = this.body;
-        const { id, psword } = await UserStorage.getUsersInfo(client.id);
-        
-        if (id) {
-            if (id === this.client.id && psword === client.psword) {
-                return { success: true };
-            }
-            return { success: false, mag: "비밀번호가 틀렸습니다."};
-        }
-        return { success: false, mag: "존재하지 않는 아이디 입니다."};
-    }
+  async login() {
+    const client = this.body;
+    try {
+      const { id, psword } = await UserStorage.getUserInfo(client.id);
 
-    async register() {
-        const client = this.body;
-        try {
-            const response = await UserStorage.save(client);
-            return response;
-        } catch (err) {
-            return { success: false, msg: err };
+      if (id) {
+        if (id === client.id && psword === client.psword) {
+          return { success: true };
         }
-        
+        return { success: false, msg: "비밀번호가 틀렸습니다." };
+      }
+      return { success: false, msg: "존재하지 않는 아이디입니다." };
+    } catch (err) {
+      return { success: false, msg: err };
     }
+  }
+
+  async register() {
+    const client = this.body;
+    try {
+      const response = await UserStorage.save(client);
+      return response;
+    } catch (err) {
+      return { success: false, msg: err };
+    }
+  }
 }
 
 module.exports = User;
--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,57 +1,27 @@
 "use strict";
 
-const fs = require("fs").promises;
+const db = require("../config/db");
 
 class UserStorage {
-    static #getUserInfo() {
-        const users = JSON.parse(data, id);
-                const idx = users.id.indexOf(id);
-                const usersKeys = Object.keys(users);
-                const userInfo = usersKeys.reduce((newUsers, info) => {
-                    newUsers[info] = users[info][idx];
-                    return newUsers;
-            }, {});
+  static getUserInfo(id) {
+    return new Promise((resolve, reject) => {
+      const query = "SELECT * FROM users WHERE id = ?;";
+      db.query(query, [id], (err, data) => {
+        if (err) reject(`${err}`);
+        resolve(data[0]);
+      });
+    });
+  }
 
-            return userInfo;
-    }
-
-    static #getUsers(data, isAll, fields) {
-        const users = JSON.parse(data);
-        if (isAll) return users;
-        const newUsers = fields.reduce((newUsers, field) => {
-            if (users.hasOwnProperty(field)) {
-                newUsers[field] = users[field];
-            }
-            return newUsers;
-        }, {});
-        return newUsers;
-    }
-
-    static getUsers(isAll, ...fields) {
-        return fs
-            .readFile(".src/databases/users.json")
-            .then((data) => {
-                return this.#getUsers(data, isALl, fields);
-            })
-            .catch(console.error);
-    }
-
-    static getUsersInfo(id) {
-        
-    }
-
-
-    static async save(userInfo) {
-        const data = await this.getUsers(true);
-        if (!users.id.includes(userInfo.id)) {
-            throw "이미 존재하는 아이디입니다.";
-        }
-        users.id.push(userInfo.id);
-        users.name.push(userInfo.name);
-        users.psword.push(userInfo.psword);
-        fs.writeFile("./src/databases/users/json", JSON.stringify(users));
-        return { success: true };
-    }
+  static async save(userInfo) {
+    return new Promise((resolve, reject) => {
+      const query = "INSERT INTO users(id, name, psword) VALUES(?, ?, ?);";
+      db.query(query, [userInfo.id, userInfo.name, userInfo.psword], (err) => {
+        if (err) reject(`${err}`);
+        resolve({ success: true });
+      });
+    });
+  }
 }
 
 module.exports = UserStorage;

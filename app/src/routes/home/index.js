6a4811da0e64c3003f858320f23804997856438c
patronum98 <zkrhklkl98@gmail.com>
2022-05-18 11:49:40 +0900

    사진업로드와 출력 기능 추가

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -5,11 +5,48 @@ const router = express.Router();
 
 const ctrl = require("./home.ctrl");
 
+const multer   = require('multer');
+
 router.get("/", ctrl.output.home);
 router.get("/login", ctrl.output.login);
 router.get("/register", ctrl.output.register);
 
 router.post("/login", ctrl.process.login);
 router.post("/register", ctrl.process.register);
+
+router.get("/upload",ctrl.output.upload)
+// router.post("/upload", ctrl.process.upload)
+
+var fs = require('fs');
+
+var storage  = multer.diskStorage({ // 2
+  destination(req, file, cb) {
+    cb(null, 'uploadedFiles/');
+  },
+  filename(req, file, cb) {
+    cb(null, `${Date.now()}__${file.originalname}`);
+  },
+});
+
+var uploadWithOriginalFilename = multer({ storage: storage });
+
+// router.get('/', function(req,res){
+//     res.render('upload');
+//   });
+  
+  router.post('/upload', uploadWithOriginalFilename.single('attachment'), function(req,res){ // 5
+      let test = JSON.stringify(req.file)
+      var dejson = JSON.parse(test)
+      var path = dejson.path
+      fs.readFile(path, function(err, data){
+          console.log('picture loading...');
+          res.writeHead(200);
+          res.write(data);
+          res.render('confirmation');
+          return res.write(data);
+          // res.end();    
+      });
+  });
+
 
 module.exports = router;
